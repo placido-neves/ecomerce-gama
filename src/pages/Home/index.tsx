3fba1d1c137eb0e4aecb7f2006d017784392ea97
placido-neves <placidofilho793@gmail.com>
2021-07-18 21:38:34 -0300

    final

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../../components/Button"
 import { Cards } from "../../components/Products"
 import { Google } from "../../components/Login"
+import { ServiceCards, TitleCards, BodyCards } from "../../components/ServiceCards"
 
 import psg from "../../assets/psg.jpg"
 import sp from "../../assets/sp.jpg"
@@ -23,7 +24,7 @@ export function Home() {
                     </form>
                 </div>
             </div>
-            <div className = "container">
+            <div className="container">
                 <div className="cad">
                     <p>Cadastre seu e-mail para receber promoções</p>
                     <div className="align-btn">
@@ -31,7 +32,7 @@ export function Home() {
                     </div>
                     <form>
                         <label htmlFor="">email:</label>
-                        <input type="text" placeholder = "exaple@exeaple.com"/>
+                        <input type="text" placeholder="exaple@exeaple.com" />
                         <Button>send</Button>
                     </form>
                 </div>
@@ -41,6 +42,52 @@ export function Home() {
                         <Cards title="Camisa do Sao Paulo" price="25.90" srcImg={sp}></Cards>
                         <Cards title="Camisa do Normal" price="15.90" srcImg={normal}></Cards>
                     </div>
+                </div>
+            </div>
+            <div className="nossos">
+                Nossos Serviços
+            </div>
+            <div className="services">
+
+                <ServiceCards>
+                    <TitleCards>Lorem Ipsum</TitleCards>
+                    <BodyCards>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </BodyCards>
+                </ServiceCards>
+
+                <ServiceCards>
+                    <TitleCards>Lorem Ipsum</TitleCards>
+                    <BodyCards>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </BodyCards>
+                </ServiceCards>
+
+                <ServiceCards>
+                    <TitleCards>Lorem Ipsum</TitleCards>
+                    <BodyCards>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </BodyCards>
+                </ServiceCards>
+            </div>
+
+            <div className="footer">
+                <div className="padding-footer">
+                    <div className="top-footer">
+                        <div className="redes-sociais">
+                            <i className="fab fa-facebook-f"></i>
+                            <i className="fab fa-instagram"></i>
+                            <i className="fab fa-whatsapp"></i>
+                            <i className="fab fa-linkedin-in"></i>
+                        </div>
+                        <div className="telefone-email">
+                            <i className="fas fa-phone-alt"></i>TELEFONE: (99)99999-9999
+                            <i className="far fa-envelope"></i>EMAIL: email@email.com
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom-footer ">
+                    ShopYou RECIFE-PE - Desenvolvido por Placido Neves Ferreira Filho
                 </div>
             </div>
         </>
